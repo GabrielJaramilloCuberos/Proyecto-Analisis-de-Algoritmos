a2e976788043cc9dff83627ecd1d9c839393fde1
GabrielJaramilloCuberos <gabrieljaramillocuberos@gmail.com>
2026-05-03 21:14:50 -0500

    actualizacion juego

--- Proyecto/main.py
+++ Proyecto/main.py
@@ -1,11 +1,15 @@
 import tkinter as tk
 import tableros as tablerosJuego
+import random
 
 tableroFacil = tablerosJuego.tableroFacil
 tableroMedio = tablerosJuego.tableroMedio
 tableroDificil = tablerosJuego.tableroDificil
 
 opciones = ["Nivel Facil","Nivel Medio","Nivel Dificil"]
+rect_preview = None
+color_actual = None
+selecciones = []
 
 def resolverJuego():
         print("Resolviendo...")
@@ -27,6 +31,53 @@ def getOpcion(value_inside,root):
 def opcionVolver(root):
     root.destroy()
     initGame()
+
+def clickInicial(event,tamanio):
+    global inicio, color_actual
+    fila = event.y // tamanio
+    columna = event.x // tamanio
+    inicio = (fila,columna)
+
+    color_actual = random.choice(["red", "blue", "green", "yellow", "purple"])
+
+    print(inicio)
+
+def seleccionarRecuadros(event, tamanio, canvas):
+
+    global rect_preview
+
+    fila1, columna1 = inicio
+    x1 = columna1 * tamanio
+    y1 = fila1 * tamanio
+    
+    fila2 = event.y // tamanio
+    col2 = event.x // tamanio
+
+    x2 = (col2 + 1) * tamanio
+    y2 = (fila2 + 1) * tamanio
+
+    rect_preview = canvas.create_rectangle((x1,y1,x2,y2),fill=color_actual, stipple = 'gray50', width = 2)
+
+def cuadroFinal(event,canvas):
+    global inicio, rect_preview, rectangulos, color_actual
+
+    if inicio is None or rect_preview is None:
+        return
+
+    coords = canvas.coords(rect_preview)
+
+    rect = canvas.create_rectangle(
+        coords,
+        fill=color_actual,
+        stipple='gray50',
+        outline=color_actual
+    )
+
+    selecciones.append(rect)
+
+    canvas.delete(rect_preview)
+    rect_preview = None
+    inicio = None
 
 def initGame():
     root = tk.Tk()
@@ -80,6 +131,10 @@ def crearTablero(tablero, dimensionTablero, tamRecuadros):
 
     resolver = tk.Button(root, text='Resolver juego', command=resolverJuego)
     resolver.pack()
+
+    canvas.bind("<Button-1>",lambda event: clickInicial(event,tamanio))
+    canvas.bind("<B1-Motion>", lambda event: seleccionarRecuadros(event, tamanio, canvas))
+    canvas.bind("<ButtonRelease-1>",lambda event: cuadroFinal(event,canvas))
 
     root.mainloop()
 
